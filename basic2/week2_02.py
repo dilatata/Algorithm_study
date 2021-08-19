72024330093e4f12aef865645939d59bcb8eced9
juyoung <dilatata96@gmail.com>
2021-08-20 00:19:58 +0900

    week2_02

--- basic2/week2_02.py
+++ basic2/week2_02.py
@@ -15,7 +15,7 @@ citations	return
 [0, 5, 6, 9, 7, 2, 10] 5
 [0, 5, 6, 9, 7, 2, 10, 1, 4, 6, 8] 
 '''
-
+# 나의 답안 93.8
 def solution(citations):
     answer = 0
     citations.sort()
@@ -33,9 +33,38 @@ def solution(citations):
             return answer
         else:
             answer = 0
-                    
+
+# 나의답안2 yeah~~!!
+def solution(citations):
+    answer = 0
+    citations.sort()
+    for i in range(len(citations)):
+        h = len(citations)-i
+        if citations[i] >= h:
+            answer = h
+            break
+    
+    return answer                    
 
 # citations = [3, 0, 6, 1, 5]
 # print(solution(citations))
 # k = [0, 5, 6, 9, 7, 2, 1, 4, 6, 8,7]
 # print(solution(k))
+
+# 다른사람 답안
+def solution(citations):
+    answer = 0
+    # 1. citations을 오름차순으로 정렬합니다.
+    citations.sort()
+    # 2. citations 길이 n을 구합니다.
+    n = len(citations)
+    # 3. 0~n까지 다음을 반복합니다
+    for i in range(n):
+        # 1. hIndex는 n-i입니다.
+        hIndex = n-i
+        # 2. citations[i]가 hIndex보다 크거나 같으면, answer에 hIndex를 저장하고 반복을 멈춥니다. 
+        if citations[i] >= hIndex:
+            answer = hIndex
+            break
+    
+    return answer
