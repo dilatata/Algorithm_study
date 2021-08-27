9d0ec7b8eac34c8c70c003c643ede8943e27b0b4
juyoung <dilatata96@gmail.com>
2021-08-28 05:21:00 +0900

    week3_02

--- basic2/week3_02.py
+++ basic2/week3_02.py
@@ -12,9 +12,9 @@ brown	yellow	return
 8	1	[3, 3]
 24	24	[8, 6]
 '''
-def solution(brown, yellow):
-    answer = []
-    return answer
+# def solution(brown, yellow):
+#     answer = []
+#     return answer
 
 
 
@@ -27,3 +27,17 @@ def solution(brown, yellow):
             a = s//i
             if yellow == (i-1)*(a-2):
                 return[i,a]
+
+
+# 다른사람 답안 2
+def solution(brown, yellow):
+    answer = []
+    total = brown + yellow                  # a * b = total
+    for b in range(1,total+1):
+        if (total / b) % 1 == 0:            # total / b = a
+            a = total / b
+            if a >= b:                      # a >= b
+                if 2*a + 2*b == brown + 4:  # 2*a + 2*b = brown + 4 
+                    return [a,b]
+            
+    return answer
