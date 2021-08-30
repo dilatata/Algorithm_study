78bdd9a4ff67e26aa9ca555e4f09789e2c35c7b8
juyoung <dilatata96@gmail.com>
2021-08-30 23:43:41 +0900

    week3_04

--- basic2/week3_04.py
+++ basic2/week3_04.py
@@ -42,3 +42,15 @@ Junkyu
 Soong
 Taewhan
 '''
+
+# 모범 코드
+N = int(input()) ; arr = []
+
+for _ in range(N):
+    a,b,c,d = list(map(str,input().split()))
+    arr.append( [a, int(b),int(c),int(d)])
+
+arr.sort(key = lambda x : (-x[1] , x[2],-x[3],x[0]) )
+
+for i in arr :
+    print(i[0])
