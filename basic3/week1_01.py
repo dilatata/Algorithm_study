99d0e5594c2c0e51247898ada39ef6557dba54b0
juyoung <dilatata96@gmail.com>
2021-09-14 23:19:52 +0900

    week1_01

--- basic3/week1_01.py
+++ basic3/week1_01.py
@@ -45,3 +45,53 @@ B
 
 예제 2: (0,1), (0,2), (1,0), (2,0), (2,2)를 누르면 된다.
 '''
+
+# 다른 코드 분석
+import sys
+input = sys.stdin.readline
+ 
+n = int(input())
+table = []
+for i in range(n):
+    temp = list(map(int, input().split()))
+    table.append(temp)
+ 
+dx = [-1, 1, 0, 0, 0]
+dy = [0, 0, 0, -1, 1]
+ans = 1234
+for f in range(1<<n):
+    a=[]
+    for i in range(n):
+        a.append(table[i][:])
+    cnt = 0
+ 
+    for i in range(n):
+        if f & (1<<i):
+            cnt += 1
+            for k in range(5):
+                nx = i + dx[k]
+                ny = 0 + dy[k]
+                if 0 <= nx < n and 0 <= ny < n:
+                    a[ny][nx] = not a[ny][nx]
+ 
+    for i in range(1, n):
+        for j in range(n):
+            if a[i-1][j]:
+                for k in range(5):
+                    nx = j + dx[k]
+                    ny = i + dy[k]
+                    if 0 <= nx < n and 0 <= ny < n:
+                        a[ny][nx] = not a[ny][nx]
+                cnt += 1
+ 
+ 
+    can = True
+    for i in range(n):
+        if a[-1][i] == True:
+            can = False
+            break
+ 
+    if can:
+        ans = min(cnt, ans)
+ 
+print(ans if ans != 1234 else -1)
