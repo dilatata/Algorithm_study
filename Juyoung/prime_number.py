fe5138937af91ab57c951ec458dc564110ce08de
juyoung <dilatata96@gmail.com>
2021-10-03 23:37:56 +0900

    solutions2 (using combination())

--- Juyoung/prime_number.py
+++ Juyoung/prime_number.py
@@ -32,8 +32,8 @@ def prime_num(num):
         return True
 
 
-# 원소 3개 합 경우 만들기
-def solution(nums):
+# 원소 3개 합 경우 만들기 - solution1
+def solution1(nums):
     num=0
     answer = 0
     
@@ -44,4 +44,18 @@ def solution(nums):
                 if prime_num(num) == True:
                     answer += 1
 
+    return answer
+
+# 원소 3개 합 경우 만들기2 solution2
+# combinations 사용을 통해서 1줄로 압축하기
+from itertools import combinations
+# combination(): 중복을 허용하지 않고, 순서 의미가 있는 조합을 리턴
+
+def solution2(nums):
+    answer = 0
+    cmb = list(combinations(nums,3))        # nums배열을 3개씩 조합 후 list로 변경
+    for arr in cmb:
+        if prime_num(sum(arr)):       # cmb를 하나씩 가져와 sum한 값을 소수 판별 함수로 넘겨줌
+            answer += 1                     # return 값이 True이면 count(=answer) +1
+    
     return answer
