fd89427aae4d884ef470878ad469aa5d6dbb9026
dilatata <dilatata96@gmail.com>
2022-01-18 23:09:26 +0900

    3진수 뒤집기 코드 +

--- programmers_ex/Lv1/ternary.py
+++ programmers_ex/Lv1/ternary.py
@@ -19,19 +19,22 @@ result 7 229
     -> 수 뒤집기 안해줘도 되지 않을까?
     1) divmod(n,m) : 사용해서 몫과 나머지 구하기 
     2) a//b, a%b : 몫, 나머지
-    if n > 0:
+    while n > 0:
         1) n, result = divmod(n, 3)
         2) result = n%3
             n =  n//3
     n = 0 이 되면 stop 해야하고 얼마나 반복할지 알 수 없으니까 while 사용
 10진법 만드는 법 : 
     1) int(string, base)
-        int(result, 3) 이때 result 값의 type 이 string
+        int(result, 3) 이때 result 값의 type 이 string 
+            -> ''.join(map(str, list))
+                이때 list 는 뒤집지 않은 list 사용
+    
     2) 계산하기
         a = result[::-1] # list 순서 뒤집기
         Decimal = 0
         for i in a:
-            Decimal += 3^a[i] 
+            Decimal += a[i]*3**i 
 '''
 
 n=125
@@ -41,6 +44,7 @@ def solution(n):
     while n>0:
         n, re = divmod(n, 3)
         result.append(re)
+        print(n)
     print(result)
     a = result[::-1]
     print(a, type(a))
@@ -50,4 +54,36 @@ def solution(n):
         print(i,a[i], 3**i, decimal)
     return decimal
 
-print(solution(n))
+# print(solution(n))
+
+# 2
+def solution2(n):
+    result = []
+    while n>0:
+        re = n%3
+        n = n//3
+        result.append(re)
+    a = result[::-1]
+
+    decimal = 0
+    for i in range(len(a)):
+        decimal += a[i]*(3**i) 
+        # print(i,a[i], 3**i, decimal)
+    return decimal
+
+# print(solution2(n))
+
+# 3.
+def solution3(n):
+    result = []
+    while n>0:
+        re = n%3
+        n = n//3
+        result.append(re)
+    a= ''.join(map(str, result))
+    print(a)
+    decimal=int(a,3)
+
+    return decimal
+
+# print(solution3(n))
