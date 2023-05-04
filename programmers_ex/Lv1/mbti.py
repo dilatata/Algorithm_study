3ff622994ec167ad5f0c7a9aa71de9b0f2edb669
Juyoung Chung <84014277+dilatata@users.noreply.github.com>
2023-05-04 14:12:33 +0900

    Add files via upload

---
+++ programmers_ex/Lv1/mbti.py
@@ -0,0 +1,76 @@
+def solution(survey, choices):
+    answer = ''
+    survey_q = dict()
+    R = 0
+    T = 0
+    C = 0
+    F = 0
+    J = 0
+    M = 0
+    A = 0
+    N = 0
+    
+    for i in range(len(survey)):
+        survey_q[survey[i]] = choices[i]
+        
+        if survey[i] == 'RT':
+            if choices[i] - 4 < 0:
+                R += 4- choices[i]
+            if choices[i] - 4 > 0:
+                T += choices[i] - 4
+        elif survey[i] == 'TR':
+            if choices[i] - 4 < 0:
+                T += 4- choices[i]
+            if choices[i] - 4 > 0:
+                R += choices[i] - 4
+        elif survey[i] == 'CF':
+            if choices[i] - 4 < 0:
+                C += 4- choices[i]
+            if choices[i] - 4 > 0:
+                F += choices[i] - 4
+        elif survey[i] == 'FC':
+            if choices[i] - 4 < 0:
+                F += 4- choices[i]
+            if choices[i] - 4 > 0:
+                C += choices[i] - 4
+        elif survey[i] == 'JM':
+            if choices[i] - 4 < 0:
+                J += 4- choices[i]
+            if choices[i] - 4 > 0:
+                M += choices[i] - 4
+        elif survey[i] == 'MJ':
+            if choices[i] - 4 < 0:
+                M += 4- choices[i]
+            if choices[i] - 4 > 0:
+                J += choices[i] - 4
+        elif survey[i] == 'AN':
+            if choices[i] - 4 < 0:
+                A += 4- choices[i]
+            if choices[i] - 4 > 0:
+                N += choices[i] - 4
+        elif survey[i] == 'NA':
+            if choices[i] - 4 < 0:
+                N += 4- choices[i]
+            if choices[i] - 4 > 0:
+                A += choices[i] - 4
+    if R >= T:
+        answer += 'R'
+    elif T > R:
+        answer += 'T'
+        
+    if C >= F:
+        answer += 'C'
+    elif F > C:
+        answer += 'F'
+        
+    if J >= M:
+        answer += 'J'
+    elif M > J:
+        answer += 'M'
+        
+    if A >= N:
+        answer += 'A'
+    elif N > A:
+        answer += 'N'    
+    
+    return answer
